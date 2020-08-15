da1c3b2e9ae65b8a8be1e44ab0ead83230d300fc
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-15 16:18:22 +0200

    Counter DOM Proj

--- All Projects/1-color-flipper/setup/app.js
+++ All Projects/1-color-flipper/setup/app.js
@@ -1,1 +1,23 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// Colores que usaremos en SIMPLE
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+// 1) Seleccionamos:
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+// 2) Agregamos Evento:
+btn.addEventListener('click', function () {
+  // Select colors of the Array (entre 0 y 3)
+  const randomNumber = getRandom(); // First, HARDCODE el number para ver que funciona
+  //Damos dicho Number al BG
+  document.body.style.backgroundColor = colors[randomNumber];
+  //Damos dicho color al SPAN
+  color.textContent = colors[randomNumber];
+});
+// 3) CB Function para el RandomNumber
+function getRandom() {
+  return Math.floor(Math.random() * colors.length);
+}
+// Que Sucede?
+// Primero --> Seleccionamos los elementos que intervienen.
+// Segundo --> Damos vida al boton, con click nos genera un RandomNumber (floor entre 0 y la cantidad en Array)
+// Segundo BIS --> Creamos la funcion que nos dará el Numero Random.
+// Tercero --> Una vez tenemos el color: Lo usamos como BG y se lo damos a la SPAN
--- All Projects/1-color-flipper/setup/hex.js
+++ All Projects/1-color-flipper/setup/hex.js
@@ -1,1 +1,33 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// Array que contiene los items para crear el Random
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+// 1) Seleccionamos Elementos
+const btn = document.querySelector('#btn');
+const colors = document.querySelector('.color');
+//
+// 2) Evento al BTN:
+btn.addEventListener('click', function () {
+  let hexColor = '#';
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandom()];
+  }
+  colors.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+//
+// 2 BIS) pensamos el interior de la function_
+// let hexColor = '#';
+// for (let i = 0; i < 6; i++) {
+//   hexColor += hex[0];  // --> First, HARDCODE el value.
+// }
+// colors.textContent = hexColor;
+// document.body.style.backgroundColor = hexColor;
+// Que sucede?
+// HexColor --> contains # + 6 Array Numbers
+// for loop --> Iteramos del 0 al 6 (6 indexes del Array) {Add randomNumber (del array) a HexColor}
+// Damos contenido al SPAN y BG color Hex
+//
+// 3) Generamos Random Number from Array:
+function getRandom() {
+  return Math.floor(Math.random() * hex.length);
+}
+//
