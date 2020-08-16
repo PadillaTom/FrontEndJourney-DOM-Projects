bb6f56012d7d214f6711f65d8d9e224d4a2af01b
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-16 13:13:58 +0200

    modal DONE

--- All Projects/6-modal/setup/app.js
+++ All Projects/6-modal/setup/app.js
@@ -2,3 +2,18 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+// 1) Select
+const modalBtn = document.querySelector('.modal-btn');
+const modalOver = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+// 2) Event para abrir: Le tenemos que poner Open Modal
+modalBtn.addEventListener('click', function () {
+  //   console.log(modalBtn.classList); // para ver que funcione
+  modalOver.classList.toggle('open-modal');
+});
+// 3) Cerramos boton
+closeBtn.addEventListener('click', function () {
+  modalOver.classList.remove('open-modal');
+});
