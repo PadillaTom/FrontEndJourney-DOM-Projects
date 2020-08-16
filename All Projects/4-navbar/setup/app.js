255a0433d3606b6fd56b2577fb3f4c0377989d5d
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-16 09:27:31 +0200

    NavBar DONE

--- All Projects/4-navbar/setup/app.js
+++ All Projects/4-navbar/setup/app.js
@@ -3,3 +3,32 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// Conceptos Generales de NavBar:
+// Height of Links: Se modificará when click btn.
+// Conviene saber de antemano la altura que tendrán los Links (10rem)
+// Tendremos una class .links{height: 0rem;} y otra .show-links {height: 10rem}
+// Lo quen haremos es Toggle between classes. Para poner y sacar altura.
+
+// 1) Seleccionamos
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+//
+// 2) Event de practica: Modo Largo, Contais Add Remove
+// navToggle.addEventListener('click', function () {
+//   //   console.log(links.classList); // log para ver las classes dentro
+//   if (links.classList.contains('show-links')) {
+//     links.classList.remove('show-links');
+//   } else {
+//     links.classList.add('show-links');
+//   }
+// });
+//
+// 3) TOGGLE!!!!!!
+navToggle.addEventListener('click', function () {
+  links.classList.toggle('show-links');
+});
+// IMPORTANTISIMO: EN CSS --> CUANDO USEMOS MEDIA QUERY PARA BIGGER SIZE SCREENS
+// DAR UNA ALTURA A LINKS DE AUTO
+// .LINKS{ HEIGHT: AUTO }
+// SINO MANTENDRAN LOS 10REM DADOS EN SHOWLINKS
